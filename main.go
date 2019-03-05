e76351f5787121d025fd20b714f8c68e526bcd70
Neven Miculinic <neven.miculinic@gmail.com>
2019-03-05 22:40:04 +0100

    Added connectors in main

--- main.go
+++ main.go
@@ -6,13 +6,27 @@ import (
 	"github.com/nmiculinic/metricsd/pkg/metricsd"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
+	"github.com/xo/dburl"
 	"google.golang.org/grpc"
 	"net"
 )
 
 func main() {
 	addr := pflag.String("addr", "[::]:8080", "service listening address")
+	dbURL := pflag.String("dburl", "", "backend database url. See https://github.com/xo/dburl for details")
 	pflag.Parse()
+
+	db, err := dburl.Open(*dbURL)
+	if err != nil {
+		logrus.WithError(err).Error("cannot open database")
+		return
+	}
+	defer db.Close()
+	if err := db.Ping(); err != nil {
+		logrus.WithError(err).Errorln("cannot ping database")
+		return
+	}
+
 	lis, err := net.Listen("tcp", *addr)
 	if err != nil {
 		logrus.WithError(err).Fatalf("failed to listen")
@@ -22,6 +36,6 @@ func main() {
 	s := grpc.NewServer()
 	metricsd.RegisterMetricsServiceServer(s, &sql.SQLBackend{})
 	if err := s.Serve(lis); err != nil {
-		logrus.WithError(err).Fatalln("failed to serve")
+		logrus.WithError(err).Panicln("failed to serve")
 	}
 }
